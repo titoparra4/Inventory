24aeb87abf71ce0c5eb0d03fa055676df1c450dd
Tito <tito.parra4@@hotmail.com>
2020-04-10 14:36:00 +0200

    Create CRUD Product

---
+++ Business/B_Category.cs
@@ -0,0 +1,38 @@
+﻿using DataAccess;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business
+{
+    public class B_Category
+    {
+        public static List<CategoryEntity> CategoryList()
+        {
+            using (var db = new InventaryContext())
+            {
+                return db.Categories.ToList();
+            }
+        }
+
+        public void CreateCategory(CategoryEntity oCategory)
+        {
+            using (var db = new InventaryContext())
+            {
+                db.Categories.Add(oCategory);
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdateCategory(CategoryEntity oCategory)
+        {
+            using (var db = new InventaryContext())
+            {
+                db.Categories.Update(oCategory);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Business/B_Storage.cs
@@ -0,0 +1,41 @@
+﻿using DataAccess;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business
+{
+    public class B_Storage
+    {
+        public static List<StorageEntity> StorageList()
+        {
+            using (var db = new InventaryContext())
+            {
+                return db.Storages.ToList();
+            }
+        }
+
+
+        public static void CreateStorage(StorageEntity oStorage)
+        {
+            using (var db = new InventaryContext())
+            {
+                db.Storages.Add(oStorage);
+                db.SaveChanges();
+            }
+        }
+
+
+        public static void UpdateStorage(StorageEntity oStorage)
+        {
+            using (var db = new InventaryContext())
+            {
+                db.Storages.Update(oStorage);
+                db.SaveChanges();
+            }
+        }
+
+    }
+}
